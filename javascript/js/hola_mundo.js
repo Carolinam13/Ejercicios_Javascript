b10343e4a39313cc6e8fd7066de17e92f89470e2
Carolina Muñoz <cm7283155@gmail.com>
2022-02-07 16:00:56 -0500

    añadiendo archivos

--- javascript/js/hola_mundo.js
+++ javascript/js/hola_mundo.js
@@ -2,6 +2,18 @@
 console.log("Hola Mundo!!");
 
 /* 2 ejercicio */
-var nombre = " Sra. Carolina"
+var nombre = "Carolina";
 if (nombre) {
-console.log("Bienvenida:"+ nombre)};
+console.log("Bienvenida Sr(a): "+ nombre)};
+
+/* 3 ejercicio */
+let costoProducto = 50000
+let impuesto = 0.10
+let domicilio = 2000
+
+let valorSubtotal = costoProducto + (costoProducto * impuesto);
+let valorTotal = valorSubtotal + domicilio
+
+console.log("El costo inicial es de: "+costoProducto + " pesos");
+console.log("El valor subtotal es de: "+ valorSubtotal + " pesos");
+console.log("El valor total es de: "+ valorTotal+ " pesos");
