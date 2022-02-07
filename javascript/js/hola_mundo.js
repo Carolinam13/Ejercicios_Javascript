16c691a7fe84b4ecb840786728059e4feef7d6f4
Carolina Muñoz <cm7283155@gmail.com>
2022-02-07 17:10:01 -0500

    add javascript

--- javascript/js/hola_mundo.js
+++ javascript/js/hola_mundo.js
@@ -2,18 +2,50 @@
 console.log("Hola Mundo!!");
 
 /* 2 ejercicio */
-var nombre = "Carolina";
+let nombre = "Carolina";
 if (nombre) {
 console.log("Bienvenida Sr(a): "+ nombre)};
 
 /* 3 ejercicio */
-let costoProducto = 50000
-let impuesto = 0.10
-let domicilio = 2000
+let costoProducto = 50000;
+let impuesto = 0.10;
+let domicilio = 2000;
 
 let valorSubtotal = costoProducto + (costoProducto * impuesto);
-let valorTotal = valorSubtotal + domicilio
+let valorTotal = valorSubtotal + domicilio;
 
-console.log("El costo inicial es de: "+costoProducto + " pesos");
-console.log("El valor subtotal es de: "+ valorSubtotal + " pesos");
-console.log("El valor total es de: "+ valorTotal+ " pesos");
+console.log("El costo inicial es de: $"+costoProducto + " pesos");
+console.log("El valor subtotal es de: $"+ valorSubtotal + " pesos");
+console.log("El valor total es de: $"+ valorTotal+ " pesos");
+
+
+/* 4 ejercicio
+declarar variable costo inicial 
+si el producto es menor a 30000 no tendra descuento 
+si el producto es igual o superior a 30000 e inferior a 100000 tendra un descuento del 5% 
+si el producto es = o superior a 1000000 el descuento sera de 10%
+imprimir valoe inicial
+y valor final,, incluido el descuento*/
+
+let costoinicial = 120000;
+let descuento1 = .05;
+let descuento2 = .10;
+
+let producto1 = costoinicial - (costoinicial * descuento1);
+let producto2 = costoinicial - (costoinicial * descuento2);
+
+console.log( "Valor inicial $"+ costoinicial );
+
+if (costoinicial < 30000){
+console.log("No tiene descuento")
+}
+
+if (costoinicial > 30000 & costoinicial < 100000){
+console.log("El valor final es de: $" + producto1);
+}
+if (costoinicial >= 100000); {
+console.log("El valor final es de: $" + producto2);
+}
+
+
+
