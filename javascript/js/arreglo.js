bab3e3b8ffb1cb7ca12e1b075a990853737ec59e
Carolina Muñoz <cm7283155@gmail.com>
2022-02-09 13:47:23 -0500

    añadiendo arreglo

---
+++ javascript/js/arreglo.js
@@ -0,0 +1,11 @@
+"use strict "
+
+let usuario = new Array ();
+usuario[0] = "Carolina";
+usuario[1] = "Liliana";
+usuario[2] = "Alexander";
+usuario[3] = "Camilo";
+
+for (var j = 0; j <= usuario.length -1; j++) {
+    console.log (usuario[j])
+}
