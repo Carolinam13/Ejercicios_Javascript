3462cffc303c456b941c558980def76f4bf18a02
Carolina Muñoz <cm7283155@gmail.com>
2022-02-21 14:01:30 -0500

    add ejercicios

---
+++ javascript/js/hola.js
@@ -0,0 +1,9 @@
+function intercambiar() {
+   document.getElementById("dark").src = "img/darkx100.jpg";
+}
+function inter() {
+    document.getElementById("reasons").src = "img/13x1000.jpg";
+ }
+ function interc() {
+    document.getElementById("stranger").src = "img/strangerx1000.jpg";
+ }
